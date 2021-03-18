380e5af91b49f1de76a0cfc25ea5e38425b26ed1
mwg2202 <mwg2202@yahoo.com>
2021-03-17 23:39:23 -0400

    Created repository and started project
    Created two main folders in the project: one that holds the python code for creating and training the ai, and one that holds the electron application

---
+++ training/flake.nix
@@ -0,0 +1,9 @@
+{
+  outputs = {self, nixpkgs}: {
+    devShell.x86_64-linux = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in pkgs.mkShell {
+      buildInputs = with pkgs; [python3];
+    };
+  };
+}
