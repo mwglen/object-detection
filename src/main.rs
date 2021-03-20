ebf0be133789a55941c8087e5c09afa64838919c
mwg2202 <mwg2202@yahoo.com>
2021-03-20 16:22:26 -0400

    Added code to loop through through the training images

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 #![allow(unused_imports)]
 
 use std::fs;
+
 use text_io::read;
 
 mod images;
@@ -13,17 +14,28 @@ use features::Feature;
 
 fn main() {
 
-    println!("Enter the directory containing test images of faces:");
-    let dir: String = read!("{}\n");
+    println!("Training using the images found in ./images");
+    println!("This may take some time...");
+    let data = find_features();
+    let data = serde_json::to_string(&data).unwrap();
+    fs::write("features.json", &data).expect("Unable to write to file");
 
-    println!("Training. This may take some time...");
-
-    let data = find_features(&dir);
-    //let data = fs::write(file, &data).expect("Unable to write to file");
-
-    //fs::write("features.json", &data).expect("Unable to write file");
 }
 
-fn find_features(dir: &String) -> Vec::<Feature> {
+fn find_features() -> Vec::<Feature> {
+    println!("{} images found", fs::read_dir("images").unwrap().count());
+
+    let paths = fs::read_dir("images").unwrap();
+    for path in paths {
+        println!("Name: {}", path.unwrap().path().display());
+    }
     unimplemented!();
 }
+
+/*
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+}
+*/
