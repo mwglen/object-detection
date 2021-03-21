dd4c7386ec4c5190c000179c70356c976bc4fdbd
mwg2202 <mwg2202@yahoo.com>
2021-03-21 16:15:46 -0400

    Reorganized functions into modules to match subcommands defined in cli.yml

--- src/features.rs
+++ src/features.rs
@@ -1,9 +1,82 @@
-extern crate serde;
-use serde::{Serialize, Deserialize};
+use super::IntegralImage;
 use std::fs;
+use serde::{Deserialize, Serialize};
+use image::io::Reader as ImageReader;
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Feature {
-    length: u16,
-    width: u16,
-} 
+pub fn main() {
+    let data = find_features();
+
+    // Output the data
+    let data = serde_json::to_string(&data).unwrap();
+    fs::write("features.json", &data).expect("Unable to write to file");
+}
+
+fn find_features() -> Vec::<Feature> {
+    
+    // Store the training images into a vector after resizing them
+    // to 24x24 and changing them to greyscale Integral Images
+    let count = fs::read_dir("images").unwrap().count();
+    println!("{} images found in ./images", count);
+    let mut images = Vec::<IntegralImage>::with_capacity(count);
+    for path in fs::read_dir("images").unwrap() {
+        // Open the image
+        let img = ImageReader::open(path.unwrap().path()).unwrap().decode().unwrap();
+        
+        // Turn the image into an Integral Image
+        let img = IntegralImage::new(img);
+
+        // Add the image to images
+        images.push(img);
+    }
+    println!("Finished gathering images");
+    
+    
+    println!("Getting a list of features");
+    let features = Vec::<TwoRectFeature>::with_capacity(100_000);
+    /*
+    // Calculate two rectangle features
+    for start_y in 0..23 {
+        for start_x in 0..22 {
+            for end_y in (start_y+1)..24 {
+                for end_x in (start_x+2)..24 {
+                    for mid_x in (start_x+1)..(24-end_x) {
+                        // Calculate horizontal features
+                        let area = img.rectangle_sum(start_x, start_y, mid_x, end_y) as isize 
+                            - img.rectangle_sum(mid_x, start_y, end_x, end_y) as isize;
+
+                        // If the area of the rectangle is greater than the threshold
+                        if (isFace==false) features.fale_positive++;
+                        if (isFace==true) features.false_negatives++;
+
+                        // Calculate vertical features
+                        let area = img.rectangle_sum(start_y, start_x, mid_x, end_x) as isize
+                                - img.rectangle_sum(mid_x, start_x, end_y, end_x)
+                        if (isFace==false) features.fale_positive++;
+                        if (isFace==true && area > 0) features.false_negatives++;
+                    }
+                }
+            }
+        }
+    }
+        
+    // Get a vector for three rectangle features
+
+    // Get a vector for four rectangle features
+    */
+    unimplemented!();
+}
+
+struct TwoRectFeature {
+    start_x: u8,
+    start_y: u8,
+    mid_x: u8,
+    end_x: u8,
+    end_y: u8,
+    false_negatives: u32,
+    false_positives: u32,
+    horizontal: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Feature {
+}
--- src/main.rs
+++ src/main.rs
@@ -2,91 +2,59 @@
 #![allow(unused_variables)]
 #![allow(unused_imports)]
 
-pub mod images;
-use images::IntegralImage;
 mod features;
-use features::Feature;
-
-use std::fs;
-use text_io::{read, scan};
-use image::{GrayImage, io::Reader as ImageReader};
+mod recognize;
+mod detect;
+mod map;
 
 extern crate clap;
 use clap::{load_yaml, App};
 
-fn main() {
+use image::{GrayImage, DynamicImage, ImageBuffer, GenericImageView, Luma};
+use image::imageops::FilterType;
 
+use serde::{Serialize, Deserialize};
+
+fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
-
-    let data = find_features();
-    let data = serde_json::to_string(&data).unwrap();
-    fs::write("features.json", &data).expect("Unable to write to file");
-
 }
 
-fn find_features() -> Vec::<Feature> {
-    
-    // Store the training images into a vector after resizing them
-    // to 24x24 and changing them to greyscale Integral Images
-    let count = fs::read_dir("images").unwrap().count();
-    println!("{} images found in ./images", count);
-    let mut images = Vec::<IntegralImage>::with_capacity(count);
-    for path in fs::read_dir("images").unwrap() {
-        // Open the image
-        let img = ImageReader::open(path.unwrap().path()).unwrap().decode().unwrap();
-        
-        // Turn the image into an Integral Image
-        let img = IntegralImage::new(img);
 
-        // Add the image to images
-        images.push(img);
-    }
-    println!("Finished gathering images");
-    
-    
-    println!("Getting a list of features");
-    let features = Vec::<TwoRectFeature>::with_capacity(100_000);
-    /*
-    // Calculate two rectangle features
-    for start_y in 0..23 {
-        for start_x in 0..22 {
-            for end_y in (start_y+1)..24 {
-                for end_x in (start_x+2)..24 {
-                    for mid_x in (start_x+1)..(24-end_x) {
-                        // Calculate horizontal features
-                        let area = img.rectangle_sum(start_x, start_y, mid_x, end_y) as isize 
-                            - img.rectangle_sum(mid_x, start_y, end_x, end_y) as isize;
 
-                        // If the area of the rectangle is greater than the threshold
-                        if (isFace==false) features.fale_positive++;
-                        if (isFace==true) features.false_negatives++;
+pub struct IntegralImage {
+    pixels: Vec<usize>,
+} impl IntegralImage {
+    pub fn new(img: DynamicImage) -> IntegralImage {
+        // Resize the image and turn it to grayscale
+        let mut img = img.resize(24, 24, FilterType::Triangle).into_luma8();
 
-                        // Calculate vertical features
-                        let area = img.rectangle_sum(start_y, start_x, mid_x, end_x) as isize
-                                - img.rectangle_sum(mid_x, start_x, end_y, end_x)
-                        if (isFace==false) features.fale_positive++;
-                        if (isFace==true && area > 0) features.false_negatives++;
+        // Calculate each pixel of the integral image
+        let mut pixels = Vec::<usize>::with_capacity(24*24);
+        for y in 0..24 {
+            for x in 0..24 {
+                let pixel = img.get_pixel_mut(x, y)[0] as usize;
+                let x = x as usize; let y = y as usize;
+                pixels.push({
+                    if (x == 0) && (y == 0) { pixel }
+                    else if x == 0 { pixels[x + 24*(y-1)] as usize + pixel }
+                    else if y == 0 { pixels[(x-1) + 24*y] as usize + pixel } 
+                    else { pixels[(x-1) + 24*y] as usize
+                            + (pixels[x + 24*(y-1)] as usize)
+                            + pixel - (pixels[(x-1) + 24*(y-1)] as usize)
                     }
-                }
+                })
             }
         }
+        IntegralImage {
+            pixels,
+        }
+
     }
-        
-    // Get a vector for three rectangle features
-
-    // Get a vector for four rectangle features
-    */
-    unimplemented!();
-}
-
-struct TwoRectFeature {
-    start_x: u8,
-    start_y: u8,
-    mid_x: u8,
-    end_x: u8,
-    end_y: u8,
-    false_negatives: u32,
-    false_positives: u32,
-    horizontal: bool,
+    pub fn rectangle_sum(&self, x1: u8, y1: u8, x2: u8, y2: u8) -> usize {
+        (self.pixels[(x2 as usize) + 24*(y2 as usize)] 
+            - self.pixels[(x2 as usize) + 24*(y1 as usize)] 
+            - self.pixels[(x1 as usize) + 24*(y2 as usize)] 
+            + self.pixels[(x1 as usize) + 24*(y1 as usize)]) as usize
+    }
 }
